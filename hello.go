c31ce380a5dcdc44bc2c53999ddc2a72d7d432a5
Bruno Arias <121177423+bruno4ri4s@users.noreply.github.com>
2023-09-14 23:41:58 -0300

    Agregando variables
    Se hace una prueba de las variables

--- hello.go
+++ hello.go
@@ -2,10 +2,32 @@ package main
 
 import (
 	"fmt"
+
 	"rsc.io/quote"
 )
 
 func main() {
 	fmt.Println("Hola Mundo")
 	fmt.Println(quote.Hello())
+
+	//Declaracion de variables
+	//	var firstName, lastName string
+	//	var age int
+
+	var (
+		firstName, lastName string
+		age                 int
+	)
+
+	firstName = "Alex"
+	lastName = "Rodriguez"
+	age = 27
+
+	var id, middleName = 1, "Santiago"
+
+	tel := 44231123
+	tel = 34231234
+
+	fmt.Println(" id: ", id, " nombre: ", firstName, " segundo nombre: ", middleName, " apellido: ", lastName, " edad: ", age, " telefono: ", tel)
+
 }
